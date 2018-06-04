32675fc980bdd3aae9ca9959bf6d9f07e907fc72
jxxchallenger <jxxchallenger.com>
2018-06-04 22:37:44 +0800

    update pom.xml

--- microservices-utils/src/main/java/com/hkbea/utils/context/SpringContextUtils.java
+++ microservices-utils/src/main/java/com/hkbea/utils/context/SpringContextUtils.java
@@ -4,6 +4,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+/**
+ * 
+ * @author Chen
+ *
+ */
 public class SpringContextUtils implements ApplicationContextAware {
 
 	private static ApplicationContext applicationContext;
