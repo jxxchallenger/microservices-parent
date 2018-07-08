6cccf21ba47f340f2f97fadbe0cfedeb9dcbfec3
jxxchallenger <jxxchallenger.com>
2018-07-08 11:06:29 +0800

    练习@SelectProvider

--- microservices-data/src/main/java/com/hkbea/data/mapper/PrivilegeMapper.java
+++ microservices-data/src/main/java/com/hkbea/data/mapper/PrivilegeMapper.java
@@ -1,5 +1,12 @@
 package com.hkbea.data.mapper;
 
+import org.apache.ibatis.annotations.SelectProvider;
+
+import com.hkbea.data.mapper.provider.PrivilegeProvider;
+import com.hkbea.data.model.SysPrivilege;
+
 public interface PrivilegeMapper {
 
+	@SelectProvider(method = "selectById", type = PrivilegeProvider.class)
+	SysPrivilege selectById(Long id);
 }
---
+++ microservices-data/src/main/java/com/hkbea/data/mapper/provider/PrivilegeProvider.java
@@ -0,0 +1,17 @@
+package com.hkbea.data.mapper.provider;
+
+import org.apache.ibatis.jdbc.SQL;
+
+public class PrivilegeProvider {
+
+	public String selectById(final Long id){
+		
+		return new SQL(){
+			{
+				SELECT("id, privilege_name, privilege_url");
+				FROM("sys_privilege");
+				WHERE("id = #{id}");
+			}
+		}.toString();
+	}
+}
